9ea1f9c5a95fb9a7405bc155bea55678ba698e6b
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2025-01-26 15:23:23 +0100

    Update

--- starlite.sh
+++ starlite.sh
@@ -47,8 +47,6 @@ validate_arguments() {
       RECORD_ID=$(grep -o '"id"[[:space:]]*:[[:space:]]*".*"' "$MANIFEST_FILE" | awk -F':' '{gsub(/[", ]/, "", $2); print $2}')
     fi
 
-    echo "RECORD_ID is $RECORD_ID"
-
     if [ -z "${RECORD_ID:-}" ]; then
       echo "❌ ERROR: RECORD_ID is not set and could not be inferred from '$MANIFEST_FILE'!" >&2
       return 1
